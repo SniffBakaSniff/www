220c8d7e29e66423ab7462756c9e2ebc261d3acf
subaka <subaka186@gmail.com>
2024-08-29 17:55:51 -0700

    Ohh Yeaaa Baby
    Looking GOOOD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { NavbarComponent } from './navbar/navbar.component';
 
 
 import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from "./about/about.component";
 
@@ -12,7 +14,7 @@ import { AboutComponent } from "./about/about.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, RouterModule, MatToolbarModule, HomeComponent, AboutComponent],
+  imports: [RouterOutlet, NavbarComponent, RouterModule, MatToolbarModule, HomeComponent, AboutComponent, MatIconModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { Router, RouterOutlet, RouterModule, RouterLinkActive } from '@angular/router';
 
 import {MatToolbarModule} from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
+import {MatButtonModule} from '@angular/material/button';
+import {MatSliderModule} from '@angular/material/slider';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterOutlet, RouterModule, RouterLinkActive, MatToolbarModule, MatButtonModule],
+  imports: [RouterOutlet, RouterModule, RouterLinkActive, MatToolbarModule, MatButtonModule, MatSliderModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
